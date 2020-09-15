7db5a34113b9e9821c706f2acc5fe98061233564
sm1l3-blip <67394271+sm1l3-blip@users.noreply.github.com>
2020-09-15 12:47:03 +0300

    Update fuzerunner.py

--- fuzerunner.py
+++ fuzerunner.py
@@ -1,3 +1,10 @@
+import sys
+import time
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+print(Fore.MAGENTA)
+print(Style.BRIGHT)
 print """coded by sm1l3
 :'######::'##::::'##::::'##:::'##::::::::'#######::
 '##... ##: ###::'###::'####::: ##:::::::'##.... ##:
@@ -12,7 +19,7 @@ print """coded by sm1l3
 
 this is url fuzzer
 
-example : url --> http://example.com/test.php?file=
+
 
 results: http://example.com/test.php?file=a
 b
@@ -21,15 +28,72 @@ d
 e
 vb.
 
+
+--thread 10 = [10 saniye aralik ile test eder]
+--proxy list.txt = [proxy listesi aktarir]
+--out log.txt = [bulunanlari kayit eder]
+--site http://example.com/test.php?file= = [hedef siteyi belirler]
+--wordlist wlist.txt = [wordlist belirler]
 """
+import getopt
+
 import requests
-url = raw_input("url --> ")
-file = raw_input("data list ---> ")
-code = input("status code ----> ")
-fo = open(file,"r+")
+
+print(Fore.RED)
+print(Style.BRIGHT)
+
+
+argv= sys.argv[1:] 
+options, args = getopt.getopt(argv, "f:l:",["thread=","out=","proxy=","wordlist=","site="])
+
+for name , value in options:
+        if name in ['--wordlist']:
+            wordlist = value
+        
+        if name in ['--site']:
+            site=value
+ 
+       
+        if name in ['--thread']:
+            thread = float(value)
+
+            
+        
+
+        if name in ['--out']:
+            out = value
+            
+        
+        if name in ['--proxy']:
+            proxy = value
+
+dos  = open(out, "w+")
+fo = open(wordlist,"r+")
+url = site
 for i in fo.readlines():
+    
+            
+    pl = open(proxy,"r")
+    for pf in pl.readlines():
+        s = requests.Session()
+        s.proxies = {"http": pf}
+        print (Fore.GREEN)
+        print (Style.BRIGHT)
+           
+    
     r = requests.get(url+i)
-    if r.status_code == code:
-       print ("found "+i)
-    else:
-       print ("not found "+i)
+    
+    time.sleep(thread)
+    
+    st = str(i)
+    stv = str(r.status_code)
+    stri = st+stv
+     
+    dos.writelines("%s\n" % stri)
+    
+    
+    print (i,+r.status_code)
+        
+    
+    
+dos.close()
